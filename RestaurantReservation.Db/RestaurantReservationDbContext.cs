cf2af28f3125ffbbcd369ebc055d8b48d2b9f338
Raghad Salem <raghadnadersalem20022017@gmail.com>
2024-11-17 21:07:00 +0200

    Database migration

--- RestaurantReservation.Db/RestaurantReservationDbContext.cs
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -1,6 +1,23 @@
-﻿namespace RestaurantReservation.Db;
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Models;
 
-public class RestaurantReservationDbContext
+namespace RestaurantReservation.Db;
+
+public class RestaurantReservationDbContext : DbContext
 {
+    private readonly string connString = @"Server=DESKTOP-33KIDRJ\SQLEXPRESS;Database=RestaurantReservationCore;Integrated Security=True; Encrypt=True; TrustServerCertificate=True;";
+    public DbSet<Customer> Customer {  get; set; }
+    public DbSet<Employee> Employee {  get; set; }
+    public DbSet<MenuItem> MenuItem {  get; set; }
+    public DbSet<Order> Order {  get; set; }
+    public DbSet<OrderItem> OrderItem {  get; set; }
+    public DbSet<Reservation> Reservation {  get; set; }
+    public DbSet<Restaurant> Restaurant {  get; set; }
+    public DbSet<Table> Table {  get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(connString);
+    }
 
 }
